669cace7c3dab9699b32d7eac37fcb06e562c79b
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-12 16:43:10 +0530

    Create dynamicKeyword.dart
    About - Dynamic keyword in Dart

---
+++ dynamicKeyword.dart
@@ -0,0 +1,18 @@
+
+
+//  dynamicKeyword -- Dynamic keyword in Dart | How to Use dynamic Keyword? -- About Dynamic keyword in Dart
+
+void main(List<String> args) {
+  
+  dynamic a = 10;
+  print(a); // --> 10
+
+  a = 5.5;
+  print(a); // --> 5.5
+
+  a = 'dart';
+  print(a); // --> dart
+
+  a = true;
+  print(a); // --> true
+}
