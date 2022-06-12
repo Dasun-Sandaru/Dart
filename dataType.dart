19bf25863b7793c6443d72c5f6c9df528c7b742c
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-12 15:05:24 +0530

    Add - dataType
    About - Data Types in Dart

---
+++ dataType.dart
@@ -0,0 +1,37 @@
+
+
+// Data Types in Dart 
+
+/*
+
+int
+double
+numbers
+string
+boolean
+list
+set
+map
+
+*/
+
+void main() {
+
+  int a = 10;
+  print(a);
+
+  double b = 5.5;
+  print(b);
+
+  String name = 'dart';
+  print(name);
+
+  bool isOk = true;
+  print(isOk);
+
+  int c = 25;
+  int d = 10;
+
+  print(c+d); // --> 35
+
+}
