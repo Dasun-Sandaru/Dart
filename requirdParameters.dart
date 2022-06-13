46fe2a95701bcff758ae565a215dceb396293897
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-13 10:06:30 +0530

    Create requirdParameters.dart
    About - Required Parameters in Dart

---
+++ requirdParameters.dart
@@ -0,0 +1,25 @@
+
+
+// requirdParameters.dart-- Required Parameters in Dart -- About Required Parameters in Dart
+
+// Types of parameters
+/*
+
+  #1. Requird Parameters
+  #2. Optional Parameters
+      2.0- optional parameter
+      2.1- named parameter
+      2.2- default parameter
+
+*/
+
+void main(List<String> args) {
+  
+  //#1. Requird Parameters
+  mySelf('Dasun', 24);
+}
+
+void mySelf(String name,int age){
+  print(name);
+  print(age);
+}
