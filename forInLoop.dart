d889753c517cc19a7fb53946f772945f4271d922
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-13 11:32:07 +0530

    Create forInLoop.dart
    About - Dart for in Loop

---
+++ forInLoop.dart
@@ -0,0 +1,18 @@
+
+// forInLoop.dart --- Dart for in Loop  --- About Dart for in Loop
+
+// syntax: for in loop
+
+  // for(variable_name in object){
+  //   //statement
+  // }
+
+void main(List<String> args) {
+  
+  var obj = [2,4,6,8,10];
+
+  for(var x in obj){
+    print(x);
+  }
+
+}
