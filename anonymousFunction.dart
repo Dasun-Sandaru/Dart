2c87472fb8b761b206cb39a0f8fd5d81b59bf3e8
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-13 11:55:05 +0530

    Create anonymousFunction.dart
    About - Anonymous Functions | Lambda Expression in Dart

---
+++ anonymousFunction.dart
@@ -0,0 +1,26 @@
+
+// anonymousFunction.dart --- Anonymous Functions or Lambda Expression in Dart  --- About Anonymous Functions or Lambda Expression in Dart
+
+// we can also create a nameless function called an Anonymous Function, or sometimes a Lambda Function
+
+// syntax: Anonymous Function
+
+  // (parameter_list){
+  //   //statement
+  // };
+
+void main(List<String> args) {
+  
+  var myName = (String name){
+    print('Name - $name');
+  };
+
+  Function age = (int age){
+    print('Age - $age');
+  };
+
+  // function call
+  myName('Dasun');
+
+  age(23);
+}
