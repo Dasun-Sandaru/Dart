a403fd52d63530ab69fa1faf450747cadc59e625
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-13 06:49:40 +0530

    Create switchCase.dart
    About - Switch Case in Dart

---
+++ switchCase.dart
@@ -0,0 +1,43 @@
+
+
+// switchCase.dart --- Switch Case Statement in Dart -- About Switch Case in Dart
+
+void main(List<String> args) {
+  
+  int date = 5;
+
+  switch(date){
+
+    case 1:
+    print('Monday');
+    break;
+
+    case 2:
+    print('Tuesday');
+    break;
+
+    case 3:
+    print('Wednesday');
+    break;
+
+    case 4:
+    print('Thursday');
+    break;
+
+    case 5:
+    print('Friday');
+    break;
+
+    case 6:
+    print('Saturday');
+    break;
+
+    case 7:
+    print('Sunday');
+    break;
+
+    default:
+    print('Invalid');
+
+  }
+}
