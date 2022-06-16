62b2cbbaf910fb26fb3d6867e2d4066242e52de9
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-16 10:16:20 +0530

    Create UpcastingAndDowncasting.dart
    About - Upcasting and Downcasting

---
+++ Oop in Dart/Access Modifier/UpcastingAndDowncasting.dart
@@ -0,0 +1,31 @@
+
+
+
+
+// Upcasting & Downcasting
+
+/*  Exmple
+
+class A{
+
+}
+
+class B extends A{
+
+}
+
+
+--- upcasting ---
+- Super class refernce එකකට Sub class Object එකක් දැමිම
+
+  A a = new B();
+
+
+--- downcasting ---
+- Super class refernce එකකට දාපු Sub class object එකක් නැවතත් Sub class refernce එකකට ම ගැනීම
+
+  B b = (B) a;
+
+
+
+*/
