5a70595c2a4c63ed168f62c72f60eb7fa6e5c038
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-16 09:22:13 +0530

    Create constructor.dart
    About - Constructor

---
+++ Oop in Dart/constructor.dart
@@ -0,0 +1,18 @@
+
+
+// - Constructor
+
+void main(List<String> args) {
+  
+  var objA = A(); // --> This is consturctor of A class
+
+}
+
+
+class A{
+
+  // create Constructor
+  A(){
+    print('This is consturctor of A class');
+  }
+}
