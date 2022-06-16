2250d115b3112e5fd2173523ed95c1ea1f5123a0
Dasun-Sandaru <dasunsandaru98@gmail.com>
2022-06-16 09:16:06 +0530

    Create classesAndObjects.dart
    About - Classes and Objects

---
+++ Oop in Dart/classesAndObjects.dart
@@ -0,0 +1,52 @@
+
+
+// - Classes and Objects
+
+void main(){
+
+  Car c1 = new Car("Red",4,4);
+  c1.drive();
+  c1.reverse();
+  c1.park();
+
+  print(c1.color);
+
+  var c2 = Car("Yellow", 2, 2);
+  print(c2.color);
+  print(c2.seats);
+  print(c2.doors);
+
+  c2.drive();
+  c2.reverse();
+  c2.park();
+
+}
+
+class Car{
+
+  String? color;
+  int? seats;
+  int? doors;
+
+  // Car(String color,int seats,int doors){
+  //   this.color = color;
+  //   this.seats = seats;
+  //   this.doors = doors;
+  // }
+  // This constructor is also work
+
+  Car(this.color,this.seats,this.doors);
+
+  void drive(){
+    print("Car is driving");
+  }
+
+  void reverse(){
+    print("Car is reversing");
+  }
+
+  void park(){
+    print("Car is parking");
+  }
+
+}
